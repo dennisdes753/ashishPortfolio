5c9e91ae953fb58f825559d7c27ec823dcc97a69
dennisdes753 <dennisdes753@gmail.com>
2024-03-14 23:40:00 +0530

    add preloader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import React, {useState, useRef, useEffect} from 'react'
 import Header from './component/header/header'
 import Banner from './component/banner/banner'
 import About from './component/about/about'
@@ -7,20 +8,57 @@ import Project from './component/projects/projects'
 import Footer from './component/footer/footer'
 
 export default function Home() {
+	const [preloader, setPreloader] = useState(true);
+	const [timer, setTimer] = useState(3);
+
+	const id = useRef<number | null>(null);
+
+	const clear = () =>{
+		window.clearInterval(id.current as number);
+		setPreloader(false);
+	};
+
+	useEffect(() => {
+		id.current = window.setInterval(() => {
+			setTimer(timer => timer - 1);
+		}, 1000);
+
+		return () => {
+			if (id.current) {
+				clearInterval(id.current);
+			}
+		};
+	}, []);
+
+	useEffect(()=>{
+		if(timer===0){
+			clear();
+		}
+	},[timer]);
+
 	return (
 		<>
-			<section>
-				<div className="main_header">	
-					<Header/>
-					<Banner/>
-				</div>	
-				<div className="after_header">
-					<About/>
-					<Recent/>
-					<Project/>
-					<Footer/>
+			{preloader ? (
+				<div className="loader-wrapper absolute">
+					<h1>Namaste</h1>
+					<h2>Welcome to My Portfolio</h2>
 				</div>
-			</section>	
+			):(
+
+					<section>
+						<div className="main_header">	
+							<Header/>
+							<Banner/>
+						</div>	
+						<div className="after_header">
+							<About/>
+							<Recent/>
+							<Project/>
+							<Footer/>
+						</div>
+					</section>
+				)
+			}	
 		</>	
 	);
 }
